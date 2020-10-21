eeb5b17fb2fd2eb01e84cb9b629be4e666b67594
Philip Søeberg <philip@soeberg.net>
2020-10-21 20:33:32 +0200

    ipxe set to a build which is proven to work\nkodi bin is now kodi-x11

--- ipxe/build.sh
+++ ipxe/build.sh
@@ -4,7 +4,7 @@ if [ ! -d "$SCRIPTPATH/ipxe.git" ]; then
   git clone https://github.com/ipxe/ipxe/ ipxe.git
 fi
 cd $SCRIPTPATH/ipxe.git
-git fetch
+git checkout ebf2eaf515e46abd43bc798e7e4ba77bfe529218
 cd src
 make bin-i386-pcbios/undionly.kpxe EMBED=$SCRIPTPATH/ipxe.embed.script
 cp -f bin-i386-pcbios/undionly.kpxe ../../ipxe.c32
