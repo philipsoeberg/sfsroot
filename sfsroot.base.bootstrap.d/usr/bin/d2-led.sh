7f314f1b9a709ab671829a35bf0e9ccbe7875219
Philip Søeberg <philip@soeberg.net>
2021-08-12 09:56:43 +0200

    using tmpfs for kodi log and rar extracts update rar vfs newest kernel have different gpio for turbot d2 led controller general updates

--- sfsroot.base.bootstrap.d/usr/bin/d2-led.sh
+++ sfsroot.base.bootstrap.d/usr/bin/d2-led.sh
@@ -2,14 +2,17 @@
 
 id=$(cat /sys/class/dmi/id/board_name 2>/dev/null)
 if [[ "x$id" == "xMinnowBoard Turbot" ]]; then
-  echo "$0: Setting D2 LED on MinnowBoard Turbot to $1"
-  cd /sys/class/gpio
-  echo 360 > export
-  cd gpio360
-  echo out > direction
-  echo 1 > value
-  cd ..
-  echo 360 > unexport
+  echo "$0: Setting D2 LED on MinnowBoard Turbot to off"
+  # as of kernel 5.x
+  gpioset 2 22=1
+  # pre kernel 5.x
+  #cd /sys/class/gpio
+  #echo 360 > export
+  #cd gpio360
+  #echo out > direction
+  #echo 1 > value
+  #cd ..
+  #echo 360 > unexport
 else
   echo "$0: Not a MinnowBoard Turbot board"
 fi
